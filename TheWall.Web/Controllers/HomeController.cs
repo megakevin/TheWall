8019afbeb1b21419f09ab5898742bc77bb5d224f
megakevin <megakevinx@gmail.com>
2013-04-19 16:34:55 -0700

    Nuevo proyecto web creado con el template de Internet Application.

---
+++ TheWall.Web/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheWall.Model.Entities;
+
+namespace TheWall.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new TheWallContext();
+            db.Countries.Add(new Country() { Name = "Kings Landing" });
+
+
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
