e59ca517f009a2167cd7ab8a8985e15cb9bad5c9
megakevin <megakevinx@gmail.com>
2013-04-19 18:23:28 -0700

    Agregado FeedbackController.

---
+++ TheWall.Web/Controllers/FeedbackController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheWall.Model.Entities;
+
+namespace TheWall.Web.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private TheWallContext db = new TheWallContext();
+
+        //
+        // GET: /Feedback/
+
+        public ActionResult Index()
+        {
+            return View(db.Feedbacks.ToList());
+        }
+
+        //
+        // GET: /Feedback/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Feedback feedback = db.Feedbacks.Find(id);
+            if (feedback == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feedback);
+        }
+
+        //
+        // GET: /Feedback/Create
+
+        public ActionResult Create(int id)
+        {
+            ViewBag.MoodSelectList = new SelectList(db.Moods, "Id", "Description");
+            return View(new Feedback() { Course = new Course() { Id = 1 }, Student = new Student() { Id = 1 } });
+        }
+
+        //
+        // POST: /Feedback/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Feedbacks.Add(feedback);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(feedback);
+        }
+
+        //
+        // GET: /Feedback/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Feedback feedback = db.Feedbacks.Find(id);
+            if (feedback == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feedback);
+        }
+
+        //
+        // POST: /Feedback/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(feedback).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(feedback);
+        }
+
+        //
+        // GET: /Feedback/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Feedback feedback = db.Feedbacks.Find(id);
+            if (feedback == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feedback);
+        }
+
+        //
+        // POST: /Feedback/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Feedback feedback = db.Feedbacks.Find(id);
+            db.Feedbacks.Remove(feedback);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
